5f65556a0f84fa0814ecf4e8b2fa2a9d8c2ffa7f
jpoot8 <jpoot.dev@gmail.com>
2022-05-07 19:33:24 -0500

    Cambio de otro usuario en el titulo

--- conector_BD.py
+++ conector_BD.py
@@ -38,6 +38,6 @@ if __name__ == '__main__':
 
     app = Application()
 
-    app.master.title('La aplicacion muestra dos')
+    app.master.title('La aplicacion muestra dos cambio de otro usuario')
 
     app.mainloop()
